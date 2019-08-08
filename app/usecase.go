aa7420347b63bc531512b8927c4a501b2a616cee
tomocy <tomocy.dev@gmail.com>
2019-08-08 18:23:20 +0900

    add (*PostUsecase).FetchPosts

--- app/usecase.go
+++ app/usecase.go
@@ -5,3 +5,7 @@ import "github.com/tomocy/matcha/domain"
 type PostUsecase struct {
 	repo domain.PostRepo
 }
+
+func (u *PostUsecase) FetchPosts() ([]*domain.Post, error) {
+	return u.repo.FetchPosts()
+}
