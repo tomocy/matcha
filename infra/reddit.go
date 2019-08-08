cea66b9644efff463c533202295d767db5e09eeb
tomocy <tomocy.dev@gmail.com>
2019-08-08 21:10:29 +0900

    add (*Reddit).FetchPosts

--- infra/reddit.go
+++ infra/reddit.go
@@ -1,7 +1,148 @@
 package infra
 
-import "golang.org/x/oauth2"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/url"
+
+	"github.com/tomocy/deverr"
+
+	"golang.org/x/oauth2"
+
+	"github.com/tomocy/matcha/domain"
+	"github.com/tomocy/matcha/infra/reddit"
+)
+
+func NewReddit() *Reddit {
+	return &Reddit{
+		oauth: oauth{
+			config: oauth2.Config{
+				ClientID:     "w9IvCG5aiZb-fA",
+				ClientSecret: "eYU7TZHoMmt1lOkOL9gbXBc2BTY",
+				RedirectURL:  "http://localhost",
+				Endpoint: oauth2.Endpoint{
+					AuthURL:   "https://www.reddit.com/api/v1/authorize",
+					TokenURL:  "https://www.reddit.com/api/v1/access_token",
+					AuthStyle: oauth2.AuthStyleInHeader,
+				},
+				Scopes: []string{
+					"read", "identity", "mysubreddits",
+				},
+			},
+		},
+	}
+}
 
 type Reddit struct {
-	oauthConfig oauth2.Config
+	oauth oauth
+}
+
+type oauth struct {
+	config oauth2.Config
+	state  string
+}
+
+func (r *Reddit) FetchPosts() ([]*domain.Post, error) {
+	tok, err := r.trieveAuthorization()
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch posts: %s", err)
+	}
+
+	var posts reddit.Posts
+	if err := r.trieve(&oauthRequest{
+		tok:     tok,
+		method:  http.MethodGet,
+		destURL: "https://oauth.reddit.com/new",
+	}, &posts); err != nil {
+		return nil, fmt.Errorf("failed to fetch posts: %s", err)
+	}
+
+	return posts.Adapt(), nil
+}
+
+func (r *Reddit) trieveAuthorization() (*oauth2.Token, error) {
+	r.oauth.state = fmt.Sprintf("%d", rand.Intn(1000))
+	url := r.oauth.config.AuthCodeURL(r.oauth.state)
+	fmt.Printf("open this link: %s\n", url)
+	tokCh, errCh := r.handleAuthorizationRedirect()
+	select {
+	case tok := <-tokCh:
+		return tok, nil
+	case err := <-errCh:
+		return nil, err
+	}
+}
+
+func (r *Reddit) handleAuthorizationRedirect() (<-chan *oauth2.Token, <-chan error) {
+	tokCh, errCh := make(chan *oauth2.Token), make(chan error)
+	go func() {
+		defer func() {
+			close(tokCh)
+			close(errCh)
+		}()
+
+		http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+			q := req.URL.Query()
+			state, code := q.Get("state"), q.Get("code")
+			if r.oauth.state != state {
+				w.WriteHeader(http.StatusBadRequest)
+				errCh <- errors.New("invalid state")
+				return
+			}
+			ctx := context.WithValue(context.Background(), oauth2.HTTPClient, &http.Client{
+				Transport: new(oauthUserAgentTransport),
+			})
+			tok, err := r.oauth.config.Exchange(ctx, code)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				errCh <- err
+				return
+			}
+
+			tokCh <- tok
+		})
+		if err := http.ListenAndServe(":80", nil); err != nil {
+			errCh <- err
+		}
+	}()
+
+	return tokCh, errCh
+}
+
+func (r *Reddit) trieve(req *oauthRequest, dest interface{}) error {
+	ctx := context.WithValue(context.Background(), oauth2.HTTPClient, &http.Client{
+		Transport: new(oauthUserAgentTransport),
+	})
+	client := r.oauth.config.Client(ctx, req.tok)
+	var resp *http.Response
+	var err error
+	switch req.method {
+	case http.MethodGet:
+		resp, err = client.Get(req.destURL)
+	default:
+		return deverr.NotImplemented
+	}
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return readJSON(resp.Body, dest)
+}
+
+type oauthRequest struct {
+	tok             *oauth2.Token
+	method, destURL string
+	params          url.Values
+}
+
+type oauthUserAgentTransport struct{}
+
+func (t *oauthUserAgentTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.Header.Set("User-Agent", "oauth-client/0.0")
+
+	return http.DefaultTransport.RoundTrip(r)
 }
