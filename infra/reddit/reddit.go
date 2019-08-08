e8fe7922197f6a0bd1bd7e089b99afdff99fea86
tomocy <tomocy.dev@gmail.com>
2019-08-08 21:10:10 +0900

    add Post to represent a post returned by reddit api

---
+++ infra/reddit/reddit.go
@@ -0,0 +1,32 @@
+package reddit
+
+import "github.com/tomocy/matcha/domain"
+
+type Posts struct {
+	Data struct {
+		Children []*struct {
+			Data Post `json:"data"`
+		} `json:"children"`
+	} `json:"data"`
+}
+
+func (ps Posts) Adapt() []*domain.Post {
+	adapteds := make([]*domain.Post, len(ps.Data.Children))
+	for i, p := range ps.Data.Children {
+		adapteds[i] = p.Data.Adapt()
+	}
+
+	return adapteds
+}
+
+type Post struct {
+	SubredditNamePrefixed string `json:"subreddit_name_prefixed"`
+	AuthorFullname        string `json:"author_fullname"`
+	Title                 string `json:"title"`
+}
+
+func (p *Post) Adapt() *domain.Post {
+	return &domain.Post{
+		Text: p.Title,
+	}
+}
