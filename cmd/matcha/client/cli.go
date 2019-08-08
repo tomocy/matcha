506644c479cfaacf940b335976a5d538d6ef6171
tomocy <tomocy.dev@gmail.com>
2019-08-09 01:46:58 +0900

    add (*CLI).PolePosts

--- cmd/matcha/client/cli.go
+++ cmd/matcha/client/cli.go
@@ -1,12 +1,40 @@
 package client
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/tomocy/matcha/domain"
 )
 
 type CLI struct{}
+
+func (c *CLI) PolePosts() error {
+	u := newPostUsecase()
+	ctx, cancelFn := context.WithCancel(context.Background())
+	sigCh := make(chan os.Signal)
+	signal.Notify(sigCh, syscall.SIGINT)
+	psCh, errCh := u.PolePosts(ctx)
+	for {
+		select {
+		case ps := <-psCh:
+			ordered := orderOlderPosts(ps)
+			c.showPosts(ordered)
+			fmt.Printf("updated at %s\n", time.Now().Format("2006/01/02 15:04"))
+		case err := <-errCh:
+			cancelFn()
+			return err
+		case sig := <-sigCh:
+			cancelFn()
+			fmt.Println(sig)
+			return nil
+		}
+	}
+}
 
 func (c *CLI) FetchPosts() error {
 	u := newPostUsecase()
