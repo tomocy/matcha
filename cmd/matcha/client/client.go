05c77e5350e484783eb25d42cc712c66af398300
tomocy <tomocy.dev@gmail.com>
2019-08-08 20:58:53 +0900

    add asciiPosts to represent a stringable posts in ascii

--- cmd/matcha/client/client.go
+++ cmd/matcha/client/client.go
@@ -1,7 +1,10 @@
 package client
 
 import (
+	"strings"
+
 	"github.com/tomocy/matcha/app"
+	"github.com/tomocy/matcha/domain"
 	"github.com/tomocy/matcha/infra"
 )
 
@@ -9,4 +12,19 @@ func newPostUsecase() *app.PostUsecase {
 	return &app.PostUsecase{
 		Repo: infra.NewReddit(),
 	}
+}
+
+type asciiPosts []*domain.Post
+
+func (ps asciiPosts) String() string {
+	var b strings.Builder
+	for i, p := range ps {
+		if i == 0 {
+			b.WriteString("-----")
+		}
+		b.WriteString(p.Text)
+		b.WriteString("-----")
+	}
+
+	return b.String()
 }
