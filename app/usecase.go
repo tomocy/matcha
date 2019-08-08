5905ce02eb3074f89cc9c634b50595bf184f7f95
tomocy <tomocy.dev@gmail.com>
2019-08-09 01:42:59 +0900

    add (*PostUsecase).PolePosts

--- app/usecase.go
+++ app/usecase.go
@@ -1,9 +1,17 @@
 package app
 
-import "github.com/tomocy/matcha/domain"
+import (
+	"context"
+
+	"github.com/tomocy/matcha/domain"
+)
 
 type PostUsecase struct {
 	Repo domain.PostRepo
+}
+
+func (u *PostUsecase) PolePosts(ctx context.Context) (<-chan []*domain.Post, <-chan error) {
+	return u.Repo.PolePosts(ctx)
 }
 
 func (u *PostUsecase) FetchPosts() ([]*domain.Post, error) {
