7fd00ba184669a16fc692bffeb7dbe471b718657
tomocy <tomocy.dev@gmail.com>
2019-08-08 22:51:59 +0900

    modify (asciiPosts).String to show created time of posts with tago

--- cmd/matcha/client/client.go
+++ cmd/matcha/client/client.go
@@ -7,6 +7,7 @@ import (
 	"github.com/tomocy/matcha/app"
 	"github.com/tomocy/matcha/domain"
 	"github.com/tomocy/matcha/infra"
+	"github.com/tomocy/tago"
 )
 
 func newPostUsecase() *app.PostUsecase {
@@ -18,12 +19,13 @@ func newPostUsecase() *app.PostUsecase {
 type asciiPosts []*domain.Post
 
 func (ps asciiPosts) String() string {
+	tago := tago.NewWithout(tago.DefaultDuration, "2006/01/02 15:04")
 	var b strings.Builder
 	for i, p := range ps {
 		if i == 0 {
 			b.WriteString("----------\n")
 		}
-		b.WriteString(fmt.Sprintf("%s %s\n%s\n%s\n", p.Subreddit, p.CreatedAt.Format("2006/01/02 15:04"), p.User.Name, p.Title))
+		b.WriteString(fmt.Sprintf("%s %s\n%s\n%s\n", p.Subreddit, tago.Ago(p.CreatedAt), p.User.Name, p.Title))
 		b.WriteString("----------\n")
 	}
 
